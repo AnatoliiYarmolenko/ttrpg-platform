990cdeb7476bd2781dd1cd97e7e5f68ff6f97b10
AnatoliiYarmolenko <ipz224_yaam@student.ztu.edu.ua>
2026-06-03 00:39:03 +0000

    chore: remove unused admin and notification API methods, clean up routes

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -12,17 +12,11 @@ router.get('/stats', adminController.getStats);
 
 router.get('/users', adminController.getUsers);
 
-router.get('/users/:id', adminController.getUserById);
-
 router.get('/campaigns', adminController.getCampaigns);
-
-router.get('/campaigns/:id', adminController.getCampaignById);
 
 router.delete('/campaigns/:id', verifyCSRFToken, adminController.deleteCampaign);
 
 router.get('/sessions', adminController.getSessions);
-
-router.get('/sessions/:id', adminController.getSessionById);
 
 router.delete('/sessions/:id', verifyCSRFToken, adminController.deleteSession);
 
